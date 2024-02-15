09235faf8639e699d8325d10da60d4abfce16122
Samip-Nepal <samipnepal5@email.com>
2024-02-15 23:00:12 +0545

    Arrow function

--- first.js
+++ first.js
@@ -1,12 +1,22 @@
-function name(){
-    console.log("Samip Nepal");
-}
-name();
-name();
-name();
- function sum(m,n) {
-  let  s=m+n;
-    return s;
+//arrow function
+ const arrow=(a,b)=>{//with arrow function
+    console.log(a+b);
  }
-let s=sum(2,3);
-console.log(s);
+ arrow(2,3);
+ let arrows=function(a,b){// without arrow function
+    console.log(a+b);
+ }
+ arrows(2,3);
+
+// function name(){
+//     console.log("Samip Nepal");
+// }
+// name();
+// name();
+// name();
+//  function sum(m,n) {
+//   let  s=m+n;
+//     return s;
+//  }
+// let s=sum(2,3);
+// console.log(s);
