653750434e4967a8110ec70f349ecb47564a366e
Samip-Nepal <samipnepal5@email.com>
2024-02-15 23:28:55 +0545

    first pratice qn

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JS-fifth-chapter
 Functions and method
+pratice qn 
+1)Create the  function using the "Funtion kkeyword that takes a string as argument & return the number of vowel in string.
--- first.js
+++ first.js
@@ -1,12 +1,30 @@
+//Pratice qn 1
+function vowels(str){
+count=0;
+for(let val of str){
+    if(val==="a"||val==="e"||val==="i"||val==="o"||val==="u")
+    count=count+1;
+    
+}
+console.log(count);
+}
+vowels("Nepal");
+
+
+
+
+
+
+
 //arrow function
- const arrow=(a,b)=>{//with arrow function
-    console.log(a+b);
- }
- arrow(2,3);
- let arrows=function(a,b){// without arrow function
-    console.log(a+b);
- }
- arrows(2,3);
+//  const arrow=(a,b)=>{//with arrow function
+//     console.log(a+b);
+//  }
+//  arrow(2,3);
+//  let arrows=function(a,b){// without arrow function
+//     console.log(a+b);
+//  }
+//  arrows(2,3);
 
 // function name(){
 //     console.log("Samip Nepal");
